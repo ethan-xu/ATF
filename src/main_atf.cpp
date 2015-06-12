9a6a611a2d673253052b440bfc8e2fd1483c8926
ethan <ethan@debian7.8.0>
2015-06-12 11:36:17 +0800

    reverse code

--- src/main_atf.cpp
+++ src/main_atf.cpp
@@ -1,9 +1,15 @@
 
 #include "common.h"
 
+int GetNum()
+{
+	return 100;
+}
 int main(int argc,char ** argv)
 {
 
 	LOG_INFO("hello ATF\n");
+	int num=GetNum();
+	LOG_INFO("here getNum=%d\n",num);
 	return 0;
 }
